fb3da9a4bc0110905537fe825b8c69e5e3745514
David R <egoz3ntrum@gmail.com>
2021-07-26 17:39:51 +0200

    auto commit by darodrig 26-07-2021 from c1r14s6

--- test.sh
+++ test.sh
@@ -1,9 +1,38 @@
 #! /bin/bash
-FILES=$(ls /bin/)
+FILES=$(find bin -type f )
 BIN=ft_nm
+COLOR_REST="$(tput sgr0)"
+COLOR_GREEN="$(tput setaf 2)" 
+COLOR_RED="$(tput setaf 3)"
 make
 for f in $FILES;do
-    diff -i <(./$BIN $f 2>/dev/null| cat -e) <(otool -t $f 2>/dev/null| cat -e)
+    otool -t "${f}" >/dev/null
+    if [ "$?" == "0" ]; then
+        diff -i <(./$BIN "${f}" 2>/dev/null| cat -e) <(otool -t "${f}" 2>/dev/null| cat -e) >/dev/null
+        if [ "$?" == "0" ];then 
+            echo -e "diff ${COLOR_GREEN}OK" $COLOR_REST "-> ${f}"
+        else
+            echo -e "diff ${COLOR_RED}K0" $COLOR_REST "-> ${f}"
+            if [ "${1}" == "-v" ]; then
+                diff -i <(./$BIN "${f}" ) <(otool -t "${f}" ) 
+            fi
+        fi    
+    fi
 done
 
+FILES=$(find /usr/bin -type f )
+for f in $FILES;do
+    otool -t "${f}" &>/dev/null
+    if [ "$?" == "0" ]; then
+        diff -i <(./$BIN "${f}" 2>/dev/null| cat -e) <(otool -t "${f}" 2>/dev/null| cat -e) >/dev/null
+        if [ "$?" == "0" ];then 
+            echo -e "diff ${COLOR_GREEN}OK" $COLOR_REST "-> ${f}"
+        else
+            echo -e "diff ${COLOR_RED}K0" $COLOR_REST "-> ${f}"
+            if [ "${1}" == "-v" ]; then
+                diff -i <(./$BIN "${f}" ) <(otool -t "${f}" ) 
+            fi
+        fi    
+    fi
+done
 #diff <(./ft_nm /bin/*) <(otool -t /bin/*) 
