dd3498b849511587b9e2d30258c67d18c9369fac
David R <egoz3ntrum@gmail.com>
2021-08-02 17:39:43 +0200

    auto commit by darodrig 02-08-2021 from c1r12s6

---
+++ nm/test.sh
@@ -0,0 +1,92 @@
+#! /bin/bash
+BIN=ft_nm
+
+if [ ! -f $BIN ];then
+    echo "${BIN} not found."
+    exit 1
+fi
+
+make
+
+if [ "$1" ] && [ "$1" != "--long" ];then
+    diff -i <(./$BIN "${1}" 2>/dev/null| cat -e) <(nm --no-sort "${1}" 2>/dev/null| cat -e) >/dev/null
+    if [ "$?" == "0" ]; then
+        echo OK "->" $1
+    else
+        diff -y <(./$BIN "${1}" 2>/dev/null| cat -e) <(nm --no-sort "${1}" 2>/dev/null| cat -e) 
+    fi
+    exit 0
+fi
+
+LEN=100
+
+FILES=$(find /usr/bin/** -type f 2>/dev/null | head -n $LEN )
+FILES_EXEC=$(find /bin/** -type f 2>/dev/null | head -n $LEN )
+FILES_O=$(find libft -name "*.o" 2>/dev/null )
+FILES_O+=$(find ~ -name "*.o" 2>/dev/null | head -n $LEN )
+FILES_SO=$(find /usr/lib/** -name "*.so" 2>/dev/null | head -n $LEN )
+FILES_DYLIB=$(find /usr/lib/** -name "*.dylib" 2>/dev/null | head -n $LEN )
+OVERKILL="/usr/lib/libnetsnmp.5.2.1.dylib /usr/lib/libnetsnmp.5.dylib"
+FILES_A=$(find . -name "*.a" | head -n $LEN)
+COLOR_REST="$(tput sgr0)"
+COLOR_GREEN="$(tput setaf 2)" 
+COLOR_RED="$(tput setaf 1)"
+COLOR_YELLOW="$(tput setaf 3)"
+
+RESULT=$(( 0 ))
+NFILES=$(( 0 ))
+
+function echo_green() {
+    echo -e ${COLOR_GREEN}$1${COLOR_REST}
+}
+
+function echo_red() {
+    echo -e ${COLOR_RED}$1${COLOR_REST}
+}
+
+function success ()
+{
+    echo_green "-------\nSUCCESS\n-------"
+}
+
+function failure ()
+{
+    echo_red "-------"
+    echo_red "FAILURE"
+    echo_red "-------"
+}
+function test_files()
+{
+    local FILES=${@}
+    if [ "$FILES" == "" ];then
+        return
+    fi
+    for f in $FILES;do
+        nm $f &>/dev/null
+        if [ "$?" == "0" ]; then
+            NFILES=$(($NFILES + 1))
+            diff -i <(./$BIN "${f}" 2>/dev/null| cat -e) <(nm --no-sort "${f}" 2>/dev/null| cat -e) >/dev/null
+            if [ "$?" == "0" ];then 
+                echo -e "diff ${COLOR_GREEN}OK" $COLOR_REST "-> ${f}"
+                RESULT=$(($RESULT + 1))
+            else
+                echo -e "diff ${COLOR_RED}K0" $COLOR_REST "-> ${f}"
+                if [ "${1}" == "-v" ]; then
+                    diff -i <(./$BIN "${f}" ) <(nm --no-sort "${f}" ) 
+                fi
+            fi
+        fi
+
+    done
+    echo "RESULT: $RESULT/$NFILES"
+    [ "${RESULT}" == "${NFILES}" ] && success || failure
+}
+# test_files $FILES_SO
+#test_files $FILES_DYLIB
+test_files $FILES_O
+test_files $FILES
+test_files $FILES_EXEC
+
+exit 0
+
+#diff <(./ft_nm /bin/*) <(otool -t /bin/*) test_files $FILES_EXEC
