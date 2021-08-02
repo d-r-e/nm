d7710cb26c25b21ec603171cdcc5bcbdfeb8a6b6
David R <egoz3ntrum@gmail.com>
2021-08-02 09:59:17 +0200

    auto commit by darodrig 02-08-2021 from c1r12s6

--- nm/src/mach.c
+++ nm/src/mach.c
@@ -55,16 +55,14 @@ const char  *get_symstr(uint32_t index)
 	const char	*ptr;
 	size_t	j;
 	ptr = g_mach.mem + g_mach.symtab.stroff;
+	printf("");
 	for (uint32_t i = 0; i < g_mach.symtab.nsyms; ++i)
 	{
+		printf("index: %d, i: %d, value: %s\n", index, i, (ptr));
 		j = ft_strlen(ptr);
-		if (i == index)
-		{
-			//printf("sym%d: %s\n", i, ptr);
-			if (ft_strlen(ptr))
-				return (ptr);
-		}
-		ptr += j + 1;
+		if (i == index && j)
+			return (ptr);
+		ptr += j+1;
 	}
 	return (NULL);
 }
@@ -78,25 +76,23 @@ int read_symtable_64(const char *mem, uint32_t nsyms)
 	if (!mem || nsyms < 0)
 		return(-1);
 	ptr = mem;
-	ft_puts("pind");
 	for (i = 0; i < nsyms; i++)
 	{
-		ft_bzero(&table, sizeof(table));
+		//ft_bzero(&table, sizeof(table));
 		ft_memcpy(&table, ptr, sizeof(table));
 		// printf("N_STAB:\t%d\n", table.n_type & N_STAB);
 		// printf("N_PEXT:\t%d\n", table.n_type & N_PEXT);
 		switch (table.n_type & N_TYPE)
 			{
 				case (N_UNDF):
-					ft_putstr("U\t");
-					get_symstr(table.n_un.n_strx);
-					ft_puts("");
+					//printf("U %s\n", get_symstr(table.n_un.n_strx));
 					break;
 				case (N_ABS):
 					ft_puts("N_ABS");
 					break;
 				case (N_SECT):
-					ft_puts("N_SECT");
+					printf("%.16llx ", table.n_value);
+					printf("T %s\n", g_mach.mem +g_mach.symtab.stroff + table.n_un.n_strx);
 					break;
 				case (N_PBUD):
 					ft_puts("N_PBUD");
@@ -118,40 +114,7 @@ struct mach_header_64 get_mach_header64(const char *memfile)
 {
 	ft_bzero(&g_mach.header, sizeof(g_mach.header));
 	ft_memcpy((void*)&g_mach.header, memfile, sizeof(g_mach.header));
-	// for(int i = 0; i < 80; ++i)
-	// 	write(1, "-", 1);
-	// write(1, "\n", 1);
-	// // printf("--Magic file: %x\n", g_mach.header.magic);
-	// switch (g_mach.header.filetype)
-	// {
-	// 	case 1:
-	// 		printf("--Type: %s\n", "object");
-	// 		break;
-	// 	case 2:
-	// 		printf("--Type: %s\n", "execut");
-	// 		break;
-	// 	case 3:
-	// 		printf("--Type: %s\n", "library");
-	// 		break;
-	// 	case 4:
-	// 		printf("--Type: %s\n", "core");
-	// 		break;
-	// 	default:
-	// 		break;
-	// }
-	// printf("--ncmds: %d\n", g_mach.header.ncmds);
-	// for(int i = 0; i < 80; ++i)
-	// 	write(1, "-", 1);
-	// write(1, "\n", 1);
 	return(g_mach.header);
-}
-
-int analyse_elf(const char *s, const char *path)
-{
-	(void)path;
-	printf("%s: %s: no symbols\n", BINARY, path);
-	(void)s;
-	return (0);
 }
 
 int analyse_mach32(void)
@@ -245,11 +208,15 @@ int	analyse_mach64(struct load_command *ptr)
 		else if (ptr->cmd == LC_DYSYMTAB)
 		{
 			struct dysymtab_command cmd;
-			(void)cmd;
-			// ft_memcpy(&cmd, ptr, sizeof(cmd));
-			// printf("LC_DYSYMTAB: to be continued...\n");
+				
+			ft_memcpy(&cmd, ptr, sizeof(cmd));
+			//printf("LC_DYSYMTAB: to be continued...\n");
 			// printf("ilocalsym: %d\n", cmd.ilocalsym);
 			// printf("nlocalsym: %d\n", cmd.nlocalsym);
+			// printf("iextdefsym: %d\n", cmd.iextdefsym);
+			// printf("nextdefsym: %d\n", cmd.nextdefsym);
+			// printf("iundefsym: %d\n", cmd.iundefsym);
+			// printf("nundefsym: %d\n", cmd.nundefsym);
 			// printf("cmdsize %u\n", cmd.cmdsize);
 			// printf("tocoff: %d\n", cmd.tocoff);
 			// printf("ntoc: %d\n", cmd.ntoc);
